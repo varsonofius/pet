c8b1f6af5b63ef3c6084f117d90a492efd08d5f1
varsonofius <129762646+varsonofius@users.noreply.github.com>
2023-07-01 06:34:06 +0300

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,7 +5,6 @@ const checkJwt = require('../auth0')
 const db = require('../db/users')
 const router = express.Router()
 
-// middleware for checking permissions (authorization)
 const checkAdmin = jwtAuthz(['read:my_private_route'], { customScopeKey: 'permissions' })
 
 // POST /api/v1/users/protected
@@ -20,19 +19,16 @@ router.post('/', async (req, res) => {
   }
 })
 
-// public - an endpoint that anyone can access
 // GET /api/v1/users/public
 router.get('/public', (req, res) => {
   res.json({ message: 'I\'m a public endpoint, any one can access me.' })
 })
 
-// protected - an endpoint that can be accessed by authenticated users
 // GET /api/v1/users/protected
 router.get('/protected', checkJwt, (req, res) => {
   res.json({ message: 'I\'m a protected route, only authenticated users can access me.' })
 })
 
-// private - an endpoint that can be accessed by users who have certain permissions
 // GET /api/v1/users/private
 router.get('/private', checkJwt, checkAdmin, (req, res) => {
   res.json({ message: 'I\'m a private route, only authorized users with \'read:my_private_route\' can access me.' })
@@ -51,18 +47,7 @@ router.get('/', (req, res) => {
     })
 })
 
-// // GET /api/v1/users/auth0|12334
-// router.get('/:id', async (req, res) => {
-//   const { id } = req.params
 
-//   try {
-//     const roles = await getUserRoles(id)
-//     res.json({ roles })
-//   } catch (error) {
-//     console.error(error)
-//     res.status(500).json({ message: 'Unable to retrieve user roles' })
-//   }
-// })
 
 // PATCH /api/v1/users/
 router.patch('/', (req, res) => {
